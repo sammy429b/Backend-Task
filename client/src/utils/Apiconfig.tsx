68eb3eba45bb5f55cc3b5dd11850b75ac09ec7c3
sammy429b <samarthnbahirgonde49@gmail.com>
2024-05-23 23:55:07 +0530

    apiconfig update

--- client/src/utils/Apiconfig.tsx
+++ client/src/utils/Apiconfig.tsx
@@ -1,15 +1,15 @@
-export class ApiConfig{
-    static transcation = `http://localhost:3000/transcations`
-    static all = `http://localhost:3000/all`
-    static stat = `http://localhost:3000/stats`
-    static pie = `http://localhost:3000/piechart`
-    static bar = `http://localhost:3000/barchart`
-}
-
 // export class ApiConfig{
-//     static transcation = `https://sales-task-backend.vercel.app/transcations`
-//     static all = `https://sales-task-backend.vercel.app/all`
-//     static stat = `https://sales-task-backend.vercel.app/stats`
-//     static pie = `https://sales-task-backend.vercel.app/piechart`
-//     static bar = `https://sales-task-backend.vercel.app/barchart`
+//     static transcation = `http://localhost:3000/transcations`
+//     static all = `http://localhost:3000/all`
+//     static stat = `http://localhost:3000/stats`
+//     static pie = `http://localhost:3000/piechart`
+//     static bar = `http://localhost:3000/barchart`
 // }
+
+export class ApiConfig{
+    static transcation = `https://sales-task-backend.vercel.app/transcations`
+    static all = `https://sales-task-backend.vercel.app/all`
+    static stat = `https://sales-task-backend.vercel.app/stats`
+    static pie = `https://sales-task-backend.vercel.app/piechart`
+    static bar = `https://sales-task-backend.vercel.app/barchart`
+}
